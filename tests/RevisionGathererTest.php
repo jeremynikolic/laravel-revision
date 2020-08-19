3112555f0bd5f84b3cb346938677f44fad038642
Jeremy Nikolic <nikolic.jeremy@gmail.com>
2020-08-19 17:47:26 +0200

    Setup testing and autoloading

---
+++ tests/RevisionGathererTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LaravelRevision\Tests;
+
+use LaravelRevision\Tests\TestCase;
+
+class RevisionGathererTest extends TestCase
+{
+
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelRevision\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+
+}
